6020b290359c69944c123e9695d8ad6380e09b45
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-05-25 04:07:11 -0700

    Console clear

--- src/resolvers/Account/branchInfo.js
+++ src/resolvers/Account/branchInfo.js
@@ -2,29 +2,30 @@ import ObjectID from "mongodb";
 import ReactionError from "@reactioncommerce/reaction-error";
 export default async function branchInfo(parent, args, context, info) {
     // console.log(parent)
-    // console.log("Have Branch",parent )
-    if (parent.branches) {
-       
-        const BranchID = parent.branches
-        if (BranchID) {
-            // console.log("BranchID", BranchID)
-            const { BranchData } = context.collections;
-            const branchDataResponse = await BranchData.find({ _id: { $in: BranchID.map(id => ObjectID.ObjectId(id)) } }).toArray();
-            // console.log("Branch Data : ", branchDataResponse)
-            // if (!branchDataResponse) {
-            //     throw new ReactionError("Branch not found");
-            // }
-            return branchDataResponse
+    // console.log("Have Branch", parent.UserRole)
+    if (parent.UserRole === 'dispatcher') {
+        if (parent.branches) {
+            const BranchID = parent.branches
+            if (BranchID) {
+                // console.log("BranchID", BranchID)
+                const { BranchData } = context.collections;
+                const branchDataResponse = await BranchData.find({ _id: { $in: BranchID.map(id => ObjectID.ObjectId(id)) } }).toArray();
+                return branchDataResponse
+            }
+            else {
+                return []
+            }
+
         }
-        else{
-            return []
-        }
-       
+        // else {
+        //     console.log("No Branch")
+        //     // return []
+        // }
     }
     else {
-        console.log("No Branch")
-        // return []
+        return []
     }
+
 
 
 }
