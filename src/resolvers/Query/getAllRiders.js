466abb224e20905ae2d05a17498c3666a6b9a1a7
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-06-15 15:20:18 +0500

    currentStatus for getAllRider API Updated

--- src/resolvers/Query/getAllRiders.js
+++ src/resolvers/Query/getAllRiders.js
@@ -18,50 +18,75 @@ import ReactionError from "@reactioncommerce/reaction-error";
  * @returns {Promise<Object>} Promise containing queried accounts
  */
 export default async function getAllRiders(_, args, context, info) {
-    if (
-        context.user === undefined ||
-        context.user === null ||
-        context.user === ""
-    ) {
-        throw new ReactionError("Access-denied", "Please Login First");
+  if (
+    context.user === undefined ||
+    context.user === null ||
+    context.user === ""
+  ) {
+    throw new ReactionError("Access-denied", "Please Login First");
+  }
+  // console.log("Current User Role : ", context.user);
+  let { branches } = args;
+  const { id } = context.user;
+  const { Accounts } = context.collections;
+  const CurrentUserRole = context.user.UserRole;
+  const CurrentUserBranch = context.user.branches;
+  let branchIds;
+  const fiveMinutesInMilliseconds = 5 * 60 * 1000; // 5 minutes in milliseconds
+  const currentTime = new Date();
+  //   console.log("Current User Role : ", context.user);
+  // console.log("Current User : ", context.user)
+  if (CurrentUserRole === "admin") {
+    const RiderAccounts = await Accounts.find({
+      UserRole: "rider",
+    }).toArray();
+    if (RiderAccounts.length === 0) {
+      throw new ReactionError("not-found", "No online rider found");
     }
-    // console.log("Current User Role : ", context.user);
-    let { branches } = args;
-    const { id } = context.user;
-    const { Accounts } = context.collections;
-    const CurrentUserRole = context.user.UserRole;
-    const CurrentUserBranch = context.user.branches;
-    let branchIds;
-    // console.log("Current User Role : ", context.user)
-    // console.log("Current User : ", context.user)
-    if (CurrentUserRole === "admin") {
-        const RiderAccounts = await Accounts.find({
-            UserRole: "rider",
-        }).toArray();
-        if (RiderAccounts.length === 0) {
-            throw new ReactionError("not-found", "No online rider found");
-        }
-        return RiderAccounts;
+    const updatedRiderAccounts = RiderAccounts.map((rider) => {
+      // Check if updatedAt is null or less than 5 minutes from the current time
+      if (
+        !rider.updatedAt ||
+        currentTime - rider.updatedAt < fiveMinutesInMilliseconds
+      ) {
+        rider.currentStatus = "offline";
+      }
+      return rider;
+    });
+    // console.log("updatedRiderAccounts ", updatedRiderAccounts);
+    return updatedRiderAccounts;
+  } else {
+    if (branches) {
+      branchIds = Array.isArray(branches) ? branches : [branches];
     } else {
-        if (branches) {
-            branchIds = Array.isArray(branches) ? branches : [branches];
-        } else {
-            branchIds = Array.isArray(CurrentUserBranch)
-                ? CurrentUserBranch
-                : [CurrentUserBranch];
-        }
-        // console.log("branchIds: ", branchIds)
-        // if (CurrentUserRole != 'rider') {
-        // if (CurrentUserRole === "dispatcher" || CurrentUserRole === "admin") {
-        const RiderAccounts = await Accounts.find({
-            UserRole: "rider",
-            branches: { $in: branchIds },
-            currentStatus: "online",
-        }).toArray();
-        // console.log(RiderAccounts)
-        if (RiderAccounts.length === 0) {
-            throw new ReactionError("not-found", "No online rider found");
-        }
-        return RiderAccounts;
+      branchIds = Array.isArray(CurrentUserBranch)
+        ? CurrentUserBranch
+        : [CurrentUserBranch];
     }
+    // console.log("branchIds: ", branchIds)
+    // if (CurrentUserRole != 'rider') {
+    // if (CurrentUserRole === "dispatcher" || CurrentUserRole === "admin") {
+    const RiderAccounts = await Accounts.find({
+      UserRole: "rider",
+      branches: { $in: branchIds },
+      currentStatus: "online",
+    }).toArray();
+    // console.log(RiderAccounts)
+    if (RiderAccounts.length === 0) {
+      throw new ReactionError("not-found", "No online rider found");
+    }
+    const updatedRiderAccounts = RiderAccounts.map((rider) => {
+      // Check if updatedAt is null or less than 5 minutes from the current time
+      if (
+        !rider.updatedAt ||
+        currentTime - rider.updatedAt < fiveMinutesInMilliseconds
+      ) {
+        rider.currentStatus = "offline";
+      }
+      return rider;
+    });
+    // console.log("updatedRiderAccounts ", updatedRiderAccounts);
+    return updatedRiderAccounts;
+    // return RiderAccounts;
+  }
 }
