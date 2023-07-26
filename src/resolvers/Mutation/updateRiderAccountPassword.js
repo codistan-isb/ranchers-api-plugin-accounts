4c03956fdbfb234d40a042832ba6c04b5471b89b
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-07-26 12:58:44 +0500

    update: updateRiderAccountPassword api

--- src/resolvers/Mutation/updateRiderAccountPassword.js
+++ src/resolvers/Mutation/updateRiderAccountPassword.js
@@ -19,67 +19,84 @@ export default async function updateRiderAccountPassword(
   ) {
     throw new ReactionError("access-denied", "please login first");
   }
-  console.log("context user ", context.user.branches);
-  const branchIdsToCheck = context?.user?.branches; // Replace with the array of branch IDs you want to check
+  if (
+    context?.user?.UserRole === "admin" ||
+    context?.user?.UserRole === "dispatcher"
+  ) {
+    let UserData;
+    // console.log("context user ", context.user.branches);
+    const branchIdsToCheck = context?.user?.branches; // Replace with the array of branch IDs you want to check
+    const { newPassword, passwordString, riderId, riderEmail } = input;
+    const now = new Date();
+    if (context?.user?.UserRole === "admin") {
+      UserData = await Accounts.findOne({
+        "emails.address": riderEmail,
+      });
+    }
+    if (context?.user?.UserRole === "dispatcher") {
+      UserData = await Accounts.findOne({
+        "emails.address": riderEmail,
+        branches: { $in: branchIdsToCheck },
+      });
+    }
 
-  const { newPassword, passwordString, riderId, riderEmail } = input;
-  const now = new Date();
-  // const UserData = await users.findOne({ "emails.address": riderEmail });
-  const UserData = await Accounts.findOne({ "emails.address": riderEmail ,"branches": { $in: branchIdsToCheck } });
+    if (!UserData) {
+      // The user document does not exist, throw an error or handle it as needed
+      throw new ReactionError("not-found", "Account not found");
+    }
+    // console.log("User Response :- ", UserData);
+    const UserID = UserData._id;
+    const hashedPass = await bcryptPassword(newPassword);
+    // console.log("hashedPass :- ", hashedPass);
 
-  if (!UserData) {
-    // The user document does not exist, throw an error or handle it as needed
-    throw new ReactionError("not-found", "Account not found");
-  }
-  console.log("User Response :- ", UserData);
-  const UserID = UserData._id;
-  const hashedPass = await bcryptPassword(newPassword);
-  console.log("hashedPass :- ", hashedPass);
+    const userUpdate = await users.updateOne(
+      { _id: UserID },
+      { $set: { "services.password.bcrypt": hashedPass, updatedAt: now } }
+    );
+    // console.log("userUpdate ", userUpdate);
+    const shop = await Shops.findOne({ shopType: "primary" });
 
-  const userUpdate = await users.updateOne(
-    { _id: UserID },
-    { $set: { "services.password.bcrypt": hashedPass, updatedAt: now } }
-  );
-  console.log("userUpdate ", userUpdate);
-  const shop = await Shops.findOne({ shopType: "primary" });
-  if (!shop) throw new ReactionError("not-found", "Shop not found");
+    if (!shop) throw new ReactionError("not-found", "Shop not found");
 
-  const dataForEmail = {
-    // Reaction Information
-    contactEmail: _.get(shop, "emails[0].address"),
-    homepage: _.get(shop, "storefrontUrls.storefrontHomeUrl", null),
-    copyrightDate: new Date().getFullYear(),
-    legalName: _.get(shop, "addressBook[0].company"),
-    physicalAddress: {
-      address: `${_.get(shop, "addressBook[0].address1")} ${_.get(
-        shop,
-        "addressBook[0].address2"
-      )}`,
-      city: _.get(shop, "addressBook[0].city"),
-      region: _.get(shop, "addressBook[0].region"),
-      postal: _.get(shop, "addressBook[0].postal"),
-    },
-    shopName: shop.name,
-    // confirmationUrl: REACTION_IDENTITY_PUBLIC_VERIFY_EMAIL_URL.replace("TOKEN", token),
-    confirmationUrl: passwordString,
-    userEmailAddress: riderEmail,
-  };
-  console.log("dataForEmail ", dataForEmail);
-  const account = await Accounts.findOne({ _id: UserData._id });
-  const bodyTemplate = "accounts/newPassword";
-  const language =
-    (account.profile && account.profile.language) || shop.language;
-  const webEmail = await context.mutations.sendEmail(context, {
-    data: dataForEmail,
-    fromShop: shop,
-    templateName: bodyTemplate,
-    language,
-    to: riderEmail,
-  });
-  console.log("webEmail ", webEmail);
-  if (webEmail) {
-    return true;
+    const dataForEmail = {
+      // Reaction Information
+      contactEmail: _.get(shop, "emails[0].address"),
+      homepage: _.get(shop, "storefrontUrls.storefrontHomeUrl", null),
+      copyrightDate: new Date().getFullYear(),
+      legalName: _.get(shop, "addressBook[0].company"),
+      physicalAddress: {
+        address: `${_.get(shop, "addressBook[0].address1")} ${_.get(
+          shop,
+          "addressBook[0].address2"
+        )}`,
+        city: _.get(shop, "addressBook[0].city"),
+        region: _.get(shop, "addressBook[0].region"),
+        postal: _.get(shop, "addressBook[0].postal"),
+      },
+      shopName: shop.name,
+      // confirmationUrl: REACTION_IDENTITY_PUBLIC_VERIFY_EMAIL_URL.replace("TOKEN", token),
+      confirmationUrl: passwordString,
+      userEmailAddress: riderEmail,
+    };
+    // console.log("dataForEmail ", dataForEmail);
+    const account = await Accounts.findOne({ _id: UserData._id });
+    const bodyTemplate = "accounts/newPassword";
+    const language =
+      (account.profile && account.profile.language) || shop.language;
+    const webEmail = await context.mutations.sendEmail(context, {
+      data: dataForEmail,
+      fromShop: shop,
+      templateName: bodyTemplate,
+      language,
+      to: riderEmail,
+    });
+    // console.log("webEmail ", webEmail);
+    if (webEmail) {
+      return true;
+    } else {
+      return null;
+    }
   } else {
-    return null;
+    throw new ReactionError("access-denied", "User not allowed");
   }
 }
