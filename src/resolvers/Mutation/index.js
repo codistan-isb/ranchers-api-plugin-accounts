134f34ad2b09dd1ab6f71a536a501ee7f86a7200
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-08-15 20:06:54 +0500

    verificationWithOTP updated

--- src/resolvers/Mutation/index.js
+++ src/resolvers/Mutation/index.js
@@ -59,7 +59,26 @@ export default {
       throw new ReactionError("not-found", "Account not found");
     }
     // console.log("User Response :- ", UserData._id)
-    const data = await sendResetPasswordOTP(context, email, UserData._id);
+    const data = await sendResetPasswordOTP(context, email, UserData?._id);
+    // console.log("Data: ", data)
+    if (data) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  async ResetPasswordWithOTP(_, { email }, context) {
+    // console.log(email)
+    const {
+      collections: { users },
+    } = context;
+    const UserData = await users.findOne({ "emails.address": email });
+    if (!UserData) {
+      // The user document does not exist, throw an error or handle it as needed
+      throw new ReactionError("not-found", "Account not found");
+    }
+    // console.log("User Response :- ", UserData._id)
+    const data = await sendResetPasswordOTP(context, email, UserData?._id);
     // console.log("Data: ", data)
     if (data) {
       return true;
